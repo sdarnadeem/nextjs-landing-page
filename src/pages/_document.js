9b397471e9c45b2f666424b8baa8102990a233a7
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-05-21 17:53:05 +0530

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import favicon from "../assets/artosyxfavicon.png";
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,6 +12,7 @@ class CustomDocument extends Document {
       <Html lang="en-US">
         <Head>
           <title>Artosyx</title>
+          <link rel="icon" type="image/png" href={favicon} sizes="16x16"></link>
         </Head>
         <body>
           <Main />
